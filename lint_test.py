8038a01d7741b868a6e7ad6b984b207e2c114005
MiroBlagoev <114353400+MiroBlagoev@users.noreply.github.com>
2022-10-22 20:59:13 +0300

    Update lint_test.py

--- lint_test.py
+++ lint_test.py
@@ -1,9 +1,42 @@
-import sys
-from pylint import lint
-THRESHOLD = 6
-run = lint.Run(["factorial.py"], do_exit=False)
-score = run.linter.stats["global_note"]
-if score < THRESHOLD:
-    print("Linter failed: Score < threshold value")
-    sys.exit(1)
-sys.exit(0)
+GitHub-action-pipeline
+
+ on:
+    push:
+       Branches: [ our-first-pipeline ]
+       paths:
+       - 'factorial.py'
+       - 'lint_test.py'
+      
+jobs:
+  lint:
+    runs-on: ubuntu-latest
+    steps:
+    - name: Test wich OS the runner is using
+      run: echo "This job is now running on ${{ runner.os }} server hosted by Github!"
+    - name: Wich branch is checked out 
+      run: echo "The name of the branch is ${{ github.ref }} in repository ${{ github.repository }}"
+    - name: Check out repository code
+     uses: sctions/checkout@v2
+     - run: echo " The ${{ github.repository }} has been cloned to the runner."
+     - name: Set up Python
+     uses: action/setup-pyton@v2
+     with: 
+      python version 3.9
+        - name: Install dependencies
+        - run: |
+          python -m pip install --upgrade pip
+          pip install pylint
+       - name: lint with pylint
+       - run: |
+            python lint_test.py
+        - name: Build container
+          run: |
+              echo "Building docker container..."
+              sleep 60
+              echo "Docker container build successfully"
+         - name: Deploy
+         - run: |
+         echo "Deploying to our imaginary cluster"
+         sleep 60
+         echo "App has been deployed sucsessfully"
+        - run: echo "This job's status is ${{ job.status }}."
